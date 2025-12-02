28d377ab67e8698700e83f0da85eb824c37c7669
Chirikumbrah <dvps4k@gmail.com>
2025-12-03 00:03:04 +0300

    added images module

---
+++ terraform/modules/images/images.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+    }
+  }
+}
+
+data "proxmox_virtual_environment_nodes" "available_nodes" {}
+
+resource "proxmox_virtual_environment_download_file" "image" {
+  for_each = toset(data.proxmox_virtual_environment_nodes.available_nodes.names)
+
+  content_type = "iso"
+  datastore_id = "local"
+  file_name    = each.image_file_name
+  node_name    = "pve"
+  url          = each.image_url
+}
+
+output "images" {
+  value = proxmox_virtual_environment_download_file.image
+}
---
+++ terraform/modules/images/variables.tf
@@ -0,0 +1,12 @@
+variable "image_url" {
+  type        = string
+  default     = null
+  description = "image URL to download"
+}
+
+variable "image_file_name" {
+  type        = string
+  default     = null
+  description = "Downloaded image file name"
+}
+
